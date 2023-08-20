f4e2ef8e2914596bd1dd347dd3698b14f6fc28e9
Pedro Petroceli <131685276+PedroPetroceli@users.noreply.github.com>
2023-08-20 17:25:28 -0300

    upando meus estudos pro github

---
+++ curso-javascript/js-para-iniciantes/Exercicio005-js/script.js
@@ -0,0 +1,21 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Pedro'
+//RESPOSTA ABAIXO \/
+//fixed
+//length
+//slice
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn')
+//REPOSTA ABAIXO \/
+//addEventListener
+//appendChild
+//innerHTML
+//id
+//outerHTML 
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
